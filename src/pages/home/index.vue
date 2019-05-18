2a4f4ed97666e601476435fede2b55d1e7a5abf5
wangyuying <wangyuying@163>
2019-05-18 15:02:39 +0800

    完成搜索条部分

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>首页</div>
+  <div>
+    <div class="searchBox">
+      <div class="search">
+        <icon type="search" size="20"></icon>
+        <span>搜索</span>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,4 +14,17 @@ export default {};
 </script>
 
 <style lang="css" scoped>
+.searchBox {
+  padding: 10px;
+  height: 30px;
+  background-color: #e8424d;
+}
+.searchBox .search {
+  background-color: #fff;
+  text-align: center;
+}
+.searchBox .search icon {
+  vertical-align: middle;
+  margin-right: 5px;
+}
 </style>
