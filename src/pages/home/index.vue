c637e633893da5824c58285c5720b22fa60d30c3
wangyuying <wangyuying@163>
2019-05-18 16:25:37 +0800

    轮播图完成

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,22 +1,41 @@
 <template>
   <div>
+    <!--搜索部分 -->
     <div class="searchBox">
       <div class="search">
         <icon type="search" size="20"></icon>
         <span>搜索</span>
       </div>
     </div>
+    <!-- 轮播图部分 -->
+    <swiper :indicator-dots="indicatorDots">
+      <swiper-item v-for="(item,index) in imgUrls" :key="index">
+        <image :src="item" class="slide-image" width="355" height="150"></image>
+      </swiper-item>
+    </swiper>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      imgUrls: [
+        "https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640",
+        "https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640",
+        "https://images.unsplash.com/photo-1551446591-142875a901a1?w=640"
+      ],
+      indicatorDots: true
+    };
+  }
+};
 </script>
 
 <style lang="css" scoped>
 .searchBox {
   padding: 10px;
   height: 30px;
+
   background-color: #e8424d;
 }
 .searchBox .search {
